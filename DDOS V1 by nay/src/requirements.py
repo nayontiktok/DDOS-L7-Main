35790d47d390bb169914703e54b3fee1e9887ec4
nayontiktok <nayontiktok2.pro@gmail.com>
2024-12-07 12:02:36 +0100

    Add files via upload

---
+++ DDOS V1 by nay/src/requirements.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+print("\ninstall les requirements pour le botnet......")
+
+if sys.platform.startswith("win"):
+    "WINDOWS"
+    os.system("pip install pystyle")
+    os.system(" pip install datetime")
+    os.system("pip install pysocks")
